a16723be54296c92d9aed9888ca6826d284cb451
oriolaguilar <aguilars99@gmail.com>
2020-05-24 12:30:00 +0200

    Set one attrubute to final

--- src/spreadsheet/Operation.java
+++ src/spreadsheet/Operation.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 public abstract class Operation implements Expression {
 
-    public Expression e1;
-    public Expression e2;
+    public final Expression e1;
+    public final Expression e2;
 
     public Operation (Expression e1, Expression e2){
         this.e1 = e1;
